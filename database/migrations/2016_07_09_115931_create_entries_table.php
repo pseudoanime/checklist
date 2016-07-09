09751576839519285402926bbe8acbc65668c292
pseudoanime <luckilaxmi@gmail.com>
2016-07-09 13:08:59 +0000

    creating migrations

---
+++ database/migrations/2016_07_09_115931_create_entries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name");
+            $table->integer('checklists_id')->unsigned();
+            $table->foreign('checklist_id')->references('id')->on('checklists')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign(['checklist_id']);
+        Schema::dropIfExists('entries');
+    }
+}
